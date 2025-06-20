1fb5a64db950a16f2d6a776b05d4bd3f69d3760b
He Fei <droxer.he@gmail.com>
2025-06-20 11:52:53 +0800

    Updated greedy algorithem.

--- greedy/greedy.go
+++ greedy/greedy.go
@@ -1,24 +1,25 @@
 package greedy
 
 func canCompleteCircuit(gas []int, cost []int) int {
-	totalGas := 0
-	totalCost := 0
-	for i := 0; i < len(gas); i++ {
-		totalGas += gas[i]
-		totalCost += cost[i]
+	n := len(gas)
+	totalTank := 0
+	currTank := 0
+	startIndex := 0
+
+	for i := 0; i < n; i++ {
+		diff := gas[i] - cost[i]
+		totalTank += diff
+		currTank += diff
+
+		if currTank < 0 {
+			startIndex = i + 1
+			currTank = 0
+		}
 	}
-	if totalGas < totalCost {
+
+	if totalTank < 0 {
 		return -1
 	}
 
-	start := 0
-	currentGas := 0
-	for i := 0; i < len(gas); i++ {
-		currentGas += gas[i] - cost[i]
-		if currentGas < 0 {
-			start = i + 1
-			currentGas = 0
-		}
-	}
-	return start
+	return startIndex
 }
